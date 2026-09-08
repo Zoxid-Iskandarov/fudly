f7ebad2d24ba1b3e94b4ed8db7abb903892d335d
Zoxid-Iskandarov <iskandarovzoxid27@gmail.com>
2026-09-08 12:58:16 +0500

    feat: add MapStruct mappers for outbox payload DTOs
    - Create ListingPublishedPayloadMapper to map Listing to ListingPublishedPayload
    - Create ListingCancelledPayloadMapper to map Listing to ListingCancelledPayload
    - Replace manual payload construction with mapper calls in ListingService

---
+++ fudly-listing-service/src/main/java/com/walking/listing/mapper/listing/ListingCancelledPayloadMapper.java
@@ -0,0 +1,14 @@
+package com.walking.listing.mapper.listing;
+
+import com.walking.listing.domain.dto.listing.ListingCancelledPayload;
+import com.walking.listing.domain.entity.listing.Listing;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface ListingCancelledPayloadMapper {
+
+    @Mapping(target = "listingId", source = "id")
+    ListingCancelledPayload toPayload(Listing listing);
+}
